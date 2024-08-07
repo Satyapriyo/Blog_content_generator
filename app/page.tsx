fe73faa58d02a4d83b51f25926960dc665850577
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2024-08-07 11:40:22 +0530

    added images in public folder

--- app/page.tsx
+++ app/page.tsx
@@ -148,16 +148,8 @@ export default function Home() {
                   </div>
                 </div>
               )}
-             
-
-             
-
-              
-              
             </div>
           </div>
-          
-         
         </div>
         {/* features */}
         <div className="mb-40 -mt-40">
@@ -166,8 +158,10 @@ export default function Home() {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {/* AI Generated Content */}
-            <div className="bg-white shadow-lg hover:bg-black 
-              text-gray-800 hover:text-white rounded-lg overflow-hidden">
+            <div
+              className="bg-white shadow-lg hover:bg-black 
+              text-gray-800 hover:text-white rounded-lg overflow-hidden"
+            >
               <div className="p-6">
                 <svg
                   className="w-12 h-12 text-blue-500 mb-4"
@@ -183,12 +177,10 @@ export default function Home() {
                     d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                   />
                 </svg>
-                <h3 className="text-xl font-semibold  mb-2">
-                  AI Generated
-                </h3>
+                <h3 className="text-xl font-semibold  mb-2">AI Generated</h3>
                 <p className="">
-                  State-of-the-art AI technology to create high-quality,
-                  unique content tailored to your needs.
+                  State-of-the-art AI technology to create high-quality, unique
+                  content tailored to your needs.
                 </p>
               </div>
             </div>
@@ -213,8 +205,8 @@ export default function Home() {
                   Fast Response
                 </h3>
                 <p className="text-gray-600">
-                  Generate blog posts in seconds, saving you time and
-                  boosting your productivity.
+                  Generate blog posts in seconds, saving you time and boosting
+                  your productivity.
                 </p>
               </div>
             </div>
@@ -247,7 +239,7 @@ export default function Home() {
           </div>
         </div>
         {/* testimonial */}
-         <Testimonials />
+        <Testimonials />
         {/* contact section */}
 
         <div
@@ -314,7 +306,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
