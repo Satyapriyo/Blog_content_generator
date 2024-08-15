5baee24e696310bfcbef3cbc208c7aee8ca1b1a6
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2024-08-15 21:24:36 +0530

    adding the hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,6 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
-
 const navigation = [
   { name: "Product", href: "" },
   { name: "Features", href: "#featurs" },
