53234d9d5a1abb77564845d93973254558d8e0f0
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2024-08-15 21:01:09 +0530

    adding the hero section

---
+++ pages/login/index.tsx
@@ -0,0 +1,14 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+const login = () =>{
+    
+    return (
+        <div>
+        <div className={inter.className}>
+        <h1 className="bg-black text-white text-center">Coming Soon   !!!!</h1>
+        </div>
+        </div>
+    )
+}
+export default login;
